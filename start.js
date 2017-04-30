f82e417cb86209e13e480962c43521daf9a5eada
moritzgoeckel <moritz.goeckel@gmail.com>
2017-04-30 14:15:17 +0200

    Toying around

--- start.js
+++ start.js
@@ -1,3 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var Botkit = require('botkit');
 
 var controller = Botkit.facebookbot({
@@ -7,11 +10,22 @@ var controller = Botkit.facebookbot({
     access_token: "EAADSUpZCVCL4BAIJZBqi1oIVsgA2QTYDsUNQiqERuZACo5kZCYG0HZAKNB6Ty6MeXZAKZBa8RWQEZCJZC2XrZBhNrrdWY4HFt4QZCDtwmkG6YqulZBWqNetvUqyKjL34ZCheco7ckONfp0BUrDH4vF7UpZAmb5DkWsyP28f6zBmJZCSi29GiwZDZD" //231252310689982
 });
 
+var webserver = express();
+webserver.use(bodyParser.json());
+webserver.use(bodyParser.urlencoded({ extended: true }));
+
+//webserver.use(express.static('public'));
+webserver.listen(process.env.PORT || 3000, null, function() {
+    console.log('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
+});
+
+controller.webserver = webserver;
+
 console.log("Running");
 
 controller.on('message_received', function(bot, message) {
     console.log("Message recieved: " + message);
-    
+
     bot.reply(message, {"attachment":{
       "type":"template",
       "payload":{
