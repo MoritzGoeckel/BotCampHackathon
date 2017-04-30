5b2729e2e5cfaac1031d4b072cf64f69929c4d7e
moritzgoeckel <moritz.goeckel@gmail.com>
2017-04-30 14:16:33 +0200

    Toying around

--- start.js
+++ start.js
@@ -15,8 +15,8 @@ webserver.use(bodyParser.json());
 webserver.use(bodyParser.urlencoded({ extended: true }));
 
 //webserver.use(express.static('public'));
-webserver.listen(process.env.PORT || 3000, null, function() {
-    console.log('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
+webserver.listen(3000, null, function() {
+    console.log('Express webserver configured and listening at http://localhost:' + 3000);
 });
 
 controller.webserver = webserver;
