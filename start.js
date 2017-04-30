49f652d5de06e5ea8164a150c9e4ad0f8ecc1085
moritzgoeckel <moritz.goeckel@gmail.com>
2017-04-30 14:03:23 +0200

    Created a test

---
+++ start.js
@@ -0,0 +1,41 @@
+var Botkit = require('botkit');
+
+var controller = Botkit.facebookbot({
+    debug: true,
+    receive_via_postback: true,
+    verify_token: "vienna_bot",
+    access_token: "EAADSUpZCVCL4BAIJZBqi1oIVsgA2QTYDsUNQiqERuZACo5kZCYG0HZAKNB6Ty6MeXZAKZBa8RWQEZCJZC2XrZBhNrrdWY4HFt4QZCDtwmkG6YqulZBWqNetvUqyKjL34ZCheco7ckONfp0BUrDH4vF7UpZAmb5DkWsyP28f6zBmJZCSi29GiwZDZD" //231252310689982
+});
+
+console.log("Running");
+
+controller.on('message_received', function(bot, message) {
+    console.log("Message recieved: " + message);
+    
+    bot.reply(message, {"attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"What do you want to do?",
+        "buttons":[
+          {
+            "type":"postback",
+            "title":"Selfie",
+            "payload":"SELFIE_ACTION"
+          },
+          {
+            "type":"postback",
+            "title":"Eat",
+            "payload":"EAT_ACTION"
+          },
+          {
+            "type":"postback",
+            "title":"Event",
+            "payload":"EVENT_ACTION"
+          }
+        ]
+      }
+    }});
+
+    //Todo
+});
